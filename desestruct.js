04b8da886b922ec0eb4298782006733c1312255d
Bruno <bruno.rp45@gmail.com>
2021-08-10 21:54:04 -0300

    desestruct e match

---
+++ desestruct.js
@@ -0,0 +1,19 @@
+const pessoa = {
+    nome: "BR",
+    sobrenome: "Ramos",
+    idade: "30",
+    profissao:"Programador"
+}
+
+// console.log(pessoa);
+
+// let nome = pessoa.nome;
+// let sobrenome = pessoa.sobrenome;
+// let idade = pessoa.idade;
+// let idade = pessoa.profissao;
+
+
+// Variáveis olhando para objeto pessoa
+let { nome, sobrenome, idade, profissao } = pessoa;
+
+console.log(nome, sobrenome, idade, profissao);
---
+++ match.js
@@ -0,0 +1,5 @@
+const textoNaoEstruturado = "Meu cpf é 123.456.569-22";
+
+const regex = new RegExp("[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}");
+
+console.log(textoNaoEstruturado.match(regex));
